b8463683c887095c2aa40b1d5bdba232ec90aa62
Lola <lolahef@gmail.com>
2019-03-22 13:27:25 -0400

    Still not very DRY, but marginally better

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -4,40 +4,42 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**
- * Main
- */
-int main(int argc, char **argv)
+char *get_path(struct dirent *entries, char *path)
 {
-  // Parse command line
-  
+  char *fullpath = malloc(strlen(path) + strlen(entries->d_name) + 2);
+  strcpy(fullpath, path);
+  strcat(fullpath, "/");
+  strcat(fullpath, entries->d_name);
+  return fullpath;
+}
+
+int main(int argc, char **argv)
+{  
   // Open directory
   char *path = argv[1];
   struct dirent *entries;
   DIR *directory;
-
   directory = opendir(path);
 
   // Read directory
   while ((entries = readdir(directory)) != NULL) {
     struct stat buf;
-    char *fullpath = malloc(strlen(path) + strlen(entries->d_name) + 2);
-    strcpy(fullpath, path);
-    strcat(fullpath, "/");
-    strcat(fullpath, entries->d_name);
+    char *fullpath = get_path(entries, path);
     stat(fullpath, &buf);
 
     if ((buf.st_mode & S_IFDIR) != 0) {
       printf("%10s    %s\n","<DIR>", entries->d_name);
+
+      // Open nested directory
       DIR *nested_directory = opendir(fullpath);
       struct dirent *nested_entries;
+
+      // Read nested directory
       while ((nested_entries = readdir(nested_directory)) != NULL) {
         struct stat buffer;
-        char *nested_path = malloc(strlen(fullpath) + strlen(nested_entries->d_name) + 2);
-        strcpy(nested_path, path);
-        strcat(nested_path, "/");
-        strcat(nested_path, nested_entries->d_name);
+        char *nested_path = get_path(nested_entries, fullpath);
         stat(nested_path, &buffer);
+
         if ((buffer.st_mode & S_IFDIR) != 0) {
           printf("%20s    %s\n","<DIR>", nested_entries->d_name);          
         } else {
@@ -45,11 +47,13 @@ int main(int argc, char **argv)
         }
       }
 
+      // Close nested directory
+      closedir(nested_directory);
     } else {
       printf("%10lld    %s\n", buf.st_size, entries->d_name);
     }
   }
-
+  
   // Close directory
   closedir(directory);
   

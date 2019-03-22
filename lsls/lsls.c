c95b21a0dcddcaf47a2ef6bf0741e89596dc2493
Lola <lolahef@gmail.com>
2019-03-22 13:02:46 -0400

    Extremely Garbage™ version of Expand the Hierarchy stretch

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -26,8 +26,25 @@ int main(int argc, char **argv)
     strcat(fullpath, "/");
     strcat(fullpath, entries->d_name);
     stat(fullpath, &buf);
+
     if ((buf.st_mode & S_IFDIR) != 0) {
       printf("%10s    %s\n","<DIR>", entries->d_name);
+      DIR *nested_directory = opendir(fullpath);
+      struct dirent *nested_entries;
+      while ((nested_entries = readdir(nested_directory)) != NULL) {
+        struct stat buffer;
+        char *nested_path = malloc(strlen(fullpath) + strlen(nested_entries->d_name) + 2);
+        strcpy(nested_path, path);
+        strcat(nested_path, "/");
+        strcat(nested_path, nested_entries->d_name);
+        stat(nested_path, &buffer);
+        if ((buffer.st_mode & S_IFDIR) != 0) {
+          printf("%20s    %s\n","<DIR>", nested_entries->d_name);          
+        } else {
+          printf("%20lld    %s\n", buffer.st_size, nested_entries->d_name);
+        }
+      }
+
     } else {
       printf("%10lld    %s\n", buf.st_size, entries->d_name);
     }

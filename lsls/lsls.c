2155a49b565c43240b70ef52a860ee3e01cfa924
Lola <lolahef@gmail.com>
2019-03-22 12:01:28 -0400

    Finished lsls printing file names and sizes

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * Main
@@ -7,12 +10,27 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
+  
   // Open directory
+  char *path = argv[1];
+  struct dirent *entries;
+  DIR *directory;
 
-  // Repeatly read and print entries
+  directory = opendir(path);
+
+  // Read directory
+  while ((entries = readdir(directory)) != NULL) {
+    struct stat buf;
+    char *fullpath = malloc(strlen(path) + strlen(entries->d_name) + 2);
+    strcpy(fullpath, path);
+    strcat(fullpath, "/");
+    strcat(fullpath, entries->d_name);
+    stat(fullpath, &buf);
+    printf("%10lld    %s\n", buf.st_size, entries->d_name);
+  }
 
   // Close directory
-
+  closedir(directory);
+  
   return 0;
 }
